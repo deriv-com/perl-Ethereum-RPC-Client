c9e433894e9a3d11c0b7b911ee86890e2a21b4ff
nima <nima@firstsource.tech>
2020-09-02 20:55:21 +0800

    Add some comments for eth json rpc response clarification

--- lib/Ethereum/RPC/Client.pm
+++ lib/Ethereum/RPC/Client.pm
@@ -43,6 +43,8 @@ sub AUTOLOAD {
     my $res = $self->http_client->post($url => json => $obj)->result;
 
     if ($res->is_error) {
+        # https://eth.wiki/json-rpc/json-rpc-error-codes-improvement-proposal
+        # the received response C<$res> contains C<code> and C<message>
         die sprintf("error code: %d, error message: %s (%s)\n", $res->code, $res->message, $method);
     }
     return $res->json->{result};
