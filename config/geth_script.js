3b3f2afca7c37bb8300ec5877e318bb7a74bed94
chylli-binary <chylli@binary.com>
2022-11-25 15:27:37 +0000

    remove useles files

--- config/geth_script.js
+++
@@ -1,6 +0,0 @@
-console.log(eth.sendTransaction({from:eth.coinbase, to:eth.accounts[0], value: web3.toWei(100, "ether")}));
- var block = eth.getBlock("latest");
-while(block.gasLimit < 7000000) {
-        console.log(eth.sendTransaction({from:eth.coinbase, to:eth.accounts[0], value: web3.toWei(1, "ether")}));
-        block = eth.getBlock("latest");
-}
